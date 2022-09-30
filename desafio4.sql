e2f459e150073594254f0f95159c7c49071a41b2
Solon <amaral.solon@hotmail.com>
2022-09-29 21:18:29 -0300

    Requisito 4

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT usuario_nome AS usuario, 
+IF(MAX(YEAR(h.historico_data)) > 2021, "Usuário ativo", "Usuário inativo") AS status_usuario
+FROM SpotifyClone.usuario AS u
+INNER JOIN SpotifyClone.historico AS h
+ON h.usuario_id = u.usuario_id
+GROUP BY u.usuario_nome
+ORDER BY u.usuario_nome;
