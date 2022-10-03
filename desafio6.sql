c5bcfc6474c3a343c839255944374370514335d3
Solon <amaral.solon@hotmail.com>
2022-10-03 13:18:12 -0300

    Ajuste Requisito 6

--- desafio6.sql
+++ desafio6.sql
@@ -4,4 +4,4 @@ CAST(MAX(plano.plano_valor) AS DECIMAL(5, 2)) AS faturamento_maximo,
 CAST(ROUND(AVG(plano.plano_valor), 2) AS DECIMAL(5, 2)) AS faturamento_medio,
 CAST(ROUND(SUM(plano.plano_valor), 2) AS DECIMAL(5, 2)) AS faturamento_total
 FROM SpotifyClone.plano AS plano
-INNER JOIN SpotifyClone.plano AS p ON p.plano_id = plano.plano_id;
+INNER JOIN SpotifyClone.usuario AS u ON u.plano_id = plano.plano_id;
